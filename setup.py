c000e9b832a4608011e3934072f47ce4fd452e7c
Mulab <chen.xm.mu@gmail.com>
2018-12-17 18:45:36 +0800

    准备v0.1.4.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ requires = [
 
 setup(
 	name = 'tuack',
-	version = '0.1.4.3',
+	version = '0.1.4.4',
 	packages = find_packages(),
 	author = 'Chen Xumin, Zhang Ruizhe, Liu Xiaoyi, Chen Junkun',
 	author_email = 'chen.xm.mu@gmail.com, 657228726@qq.com, circuitcoder0@gmail.com, 1261954105@qq.com',
