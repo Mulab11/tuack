9d171522e79edcdddc14f13411bfac34f0ad85f7
Mulab <chen.xm.mu@gmail.com>
2020-06-18 14:57:45 +0800

    v0.1.4.10发布

--- setup.py
+++ setup.py
@@ -37,10 +37,10 @@ requires = [
 
 setup(
 	name = 'tuack',
-	version = '0.1.4.9.2',
+	version = '0.1.4.10',
 	packages = find_packages(),
-	author = 'Chen Xumin, Zhang Ruizhe, Liu Xiaoyi, Chen Junkun',
-	author_email = 'chen.xm.mu@gmail.com, 657228726@qq.com, circuitcoder0@gmail.com, 1261954105@qq.com',
+	author = 'Chenxu Min, Zhang Ruizhe, Liu Xiaoyi, Chen Junkun, Chen Shengqi, Luo Lingxiao',
+	author_email = 'chen.xm.mu@gmail.com, 657228726@qq.com, circuitcoder0@gmail.com, 1261954105@qq.com, i@harrychen.xyz',
 	url = '',
 	license = 'https://git.thusaac.com/publish/tuack/blob/master/LICENSE',
 	description = 'Tools for generating an Tsinghua/OI/ICPC-styled problem or contest for multiple judges.',
