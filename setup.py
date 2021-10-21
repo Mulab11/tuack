df246edf5041077b20d0b060cfba55665ff8ddec
Mulab <chen.xm.mu@gmail.com>
2021-10-21 08:30:23 +0800

    v0.1.4.12

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ requires = [
 
 setup(
 	name = 'tuack',
-	version = '0.1.4.11',
+	version = '0.1.4.12',
 	packages = find_packages(),
 	author = 'Chenxu Min, Zhang Ruizhe, Liu Xiaoyi, Chen Junkun, Chen Shengqi, Luo Lingxiao',
 	author_email = 'chen.xm.mu@gmail.com, 657228726@qq.com, circuitcoder0@gmail.com, 1261954105@qq.com, i@harrychen.xyz',
