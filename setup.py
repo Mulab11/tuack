cef13fa2bcfc30c7fa1711b2de57820011404bcc
Mulab <chen.xm.mu@gmail.com>
2022-06-21 18:20:44 +0800

    准备v0.1.4.12.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class PostInstallCommand(install):
 		install.run(self)
 
 requires = [
-	'jinja2 >= 2.10',
+	'jinja2 >= 3.0',
 	'natsort >= 6.0.0',
 	'pyyaml >= 5.1',
 	'rarfile >= 3.0',
@@ -38,7 +38,7 @@ requires = [
 
 setup(
 	name = 'tuack',
-	version = '0.1.4.12.2',
+	version = '0.1.4.12.3',
 	packages = find_packages(),
 	author = 'Chenxu Min, Zhang Ruizhe, Liu Xiaoyi, Chen Junkun, Chen Shengqi, Luo Lingxiao, Cheng Yuxuan',
 	author_email = 'chen.xm.mu@gmail.com, 657228726@qq.com, circuitcoder0@gmail.com, 1261954105@qq.com, i@harrychen.xyz, 0.0@pku.edu.cn',
